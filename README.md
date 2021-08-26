e3d7295a007ed60bfa0976ffd4be0fa1bbb929be
Fritzchips <fritzjohn_solibio@yahoo.ca>
2021-08-25 19:17:06 -0700

    refactoring of operators and overall code

---
+++ README.md
@@ -0,0 +1,26 @@
+## Rock Paper Scissor Game
+
+Calculator made with basic HTML, CSS and JavaScript
+
+Try it out [here](https://fritzchips.github.io/Calculator/)
+
+![calulator](./images/cal_use.gif)
+
+## Instructions
+
+- Input the numbers of choice up to 10 characters.
+- Choose the operator that you want to run on the right section.
+- `C` button to clear
+- `MR` use a saved input from memory
+- `M+` save one input to memory
+- `M-` remove save input from memory
+
+## Summary
+
+I wanted to create this project to expand my knowledge of operators and event listeners. When the user inputs a value, it is looped through the `DOM` , saved as a value and displayed on the screen. The operators process the request through a `switch` and the total return value is displayed. This project has taught me to be efficient with the use of `forEach` loop.
+
+## Author
+
+---
+
+- **Fritz Solibio** - _Software Development Mastermind Student_
--- index.js
+++ index.js
@@ -1,222 +1,134 @@
-let outPut = document.querySelector('.output');
-let btnArr = Array.from(document.getElementsByClassName('btn'));
+const outPut = document.querySelector(".output");
 
-let arr = [];
-let memory = [];
-let change;
-let total = 0;
-let dotControl = true;
-let equalControl = 4;
-let memoryControl = true;
+let firstOperand = "";
+let secondOperand = "";
+let thirdOperand = "";
+let operator = "";
+let period = false;
 
+document.querySelectorAll(".number").forEach((numberButton) => {
+  numberButton.addEventListener("click", () => {
+    const number = numberButton.textContent;
 
-addEventListener('click', (e) => {
-    let item = e.target;
-
-    let find = btnArr.indexOf(item);
-   
-    switch(find){
-        case 0:
-            if(memoryControl == false){
-                arr = [];
-                arr.push(memory[0]);
-                converter();
-            } else {
-                outPut.innerText = '0';
-                arr = [];
-                equalControl = 4;
-            } 
-            break;
-        case 5:
-            if(outPut.innerText != 0){
-                memory.shift();
-                memory.push(outPut.innerText); 
-                memoryControl = false;
-            } 
-            
-            break;  
-        case 10:
-            memory.shift();
-            memoryControl = true;
-            break;    
-        //memory
-
-        case 1:
-            if(arr.length < 20) {
-            arr.push(7);
-            converter();
-            }
-            break;
-        case 2:
-            if(arr.length < 20) {
-            arr.push(4);
-            converter();
-            }
-            break;
-        case 3:
-            if(arr.length < 20) {
-            arr.push(1);
-            converter();
-            }
-            break;
-        case 4:
-
-            if(arr != '' && arr.length < 20) {
-                arr.push(0);
-                converter();
-            } else if(arr == ''){
-            outPut.innerText = 0;
-            }
-
-            break;
-        case 6:
-            if(arr.length < 20) {
-            arr.push(8);
-            converter();
-            }
-            break;
-        case 7:
-            if(arr.length < 20) {
-            arr.push(5);
-            converter();
-            }
-            break;
-        case 8:
-            if(arr.length < 20) {
-            arr.push(2);
-            converter();
-            }
-            break;
-        case 9:
-            if(arr.length < 20) {
-                if(dotControl == true) {
-                    if(arr == '') {
-                        arr.push(0);
-                    }
-                    arr.push('.');
-                    dotControl = false;
-                    converter();
-                }
-                
-            } 
-            break;
-        case 11:
-            if(arr.length <20) {
-            arr.push(9);
-            converter();
-            }
-            break;
-        case 12:
-            if(arr.length < 20) {
-            arr.push(6);
-            converter();
-            }
-            break;
-        case 13:
-            if(arr.length < 20) {
-            arr.push(3);
-            converter();
-            }
-            break;
-        //0-9 and .
-        
-        case 14:
-
-            calculate();
-            //equal
-            break;
-        case 15:
-
-            arr = [];
-            change;
-            total = 0;
-            dotControl = true;
-            outPut.innerText = 0;
-            equalControl = 4;
-            //clear
-            break;
-        case 16:
-            if(arr !=''){
-                if(equalControl == 2) {
-                    calculate();
-                } else {
-                    calculate();
-                    equalControl = 2;
-                }
-            }
-            //divide
-            break;
-        case 17:
-            if(arr != ''){
-                if(equalControl == 3) {
-                    calculate();
-                } else {
-                    calculate();
-                    equalControl = 3;
-                }
-            }
-            //multi
-            break;
-        case 18:
-            if(arr != '') {
-                if(equalControl == 1) {
-                    calculate();
-                } else {
-                    calculate();
-                    equalControl = 1;
-                }
-            }
-            //subtract
-            break;
-        case 19:
-            if(arr != '') {
-                if(equalControl == 0) {
-                    calculate();
-                } else {
-                    calculate();
-                    equalControl = 0;
-                }
-            }
-            //add
-            break;
-        // add/sub/div/multi/
-
-        }
-
-}) 
-
-function converter() {
-    change = arr.join('');
-    outPut.innerText = change;
-} 
-
-function calculate() {
-
-    switch(equalControl){
-        case 0:
-            total += parseFloat(change);
-            break;
-        case 1:
-            total -= parseFloat(change);
-            break;
-        case 2:
-            total /= parseFloat(change);
-            break;
-        case 3:
-            total *= parseFloat(change);
-            break;
-        case 4:
-            if(change == undefined) {
-                total = 0;
-            } else if(total == 0) {
-                total = parseFloat(change);
-            } else {
-                total;
-            }
-            break;
+    if (outPut.textContent.length === 10) return;
+    if (!operator) {
+      firstOperand += number;
+      outPut.textContent = firstOperand;
+    } else {
+      secondOperand += number;
+      outPut.textContent = secondOperand;
     }
-    
-    outPut.innerText = total;
-    arr = [];
-    dotControl = true;
-} 
+  });
+});
+
+document.querySelector(".period").addEventListener("click", () => {
+  if (!period && outPut.textContent.length < 10) {
+    if (!operator) {
+      if (outPut.textContent.value === "0") {
+        firstOperand = "0.";
+        outPut.textContent = firstOperand;
+      } else {
+        firstOperand += ".";
+        outPut.textContent = firstOperand;
+      }
+      return (period = true);
+    }
+
+    if (operator) {
+      if (secondOperand === "") {
+        secondOperand += "0.";
+        outPut.textContent = secondOperand;
+      } else {
+        secondOperand += ".";
+        outPut.textContent = secondOperand;
+      }
+      return (period = true);
+    }
+  }
+});
+
+document.querySelectorAll(".operator").forEach((operatorButton) => {
+  operatorButton.addEventListener("click", () => {
+    const selectedOperator = operatorButton.textContent;
+
+    if (secondOperand) {
+      const result = calculate();
+      firstOperand = result;
+      secondOperand = "";
+      period = false;
+      operator = selectedOperator;
+      outPut.textContent = result;
+    }
+    if (firstOperand) {
+      operator = selectedOperator;
+      period = false;
+      return;
+    }
+  });
+});
+
+document.querySelector(".equal").addEventListener("click", () => {
+  if (secondOperand) {
+    const result = calculate();
+    firstOperand = result;
+    secondOperand = "";
+    period = false;
+    operator = "";
+    outPut.textContent = result;
+  }
+});
+
+document.querySelector(".clear").addEventListener("click", () => {
+  firstOperand = "";
+  secondOperand = "";
+  operator = "";
+  period = false;
+  outPut.textContent = "0";
+});
+
+document.querySelectorAll(".memory").forEach((memoryButton) => {
+  memoryButton.addEventListener("click", () => {
+    const memoryOperator = memoryButton.textContent;
+    memoryActivate(memoryOperator);
+  });
+});
+
+const memoryActivate = (memoryOperator) => {
+  switch (memoryOperator) {
+    case "MR":
+      if (thirdOperand) {
+        if (!operator) {
+          firstOperand = thirdOperand;
+          return (outPut.textContent = firstOperand);
+        } else {
+          secondOperand = thirdOperand;
+          return (outPut.textContent = secondOperand);
+        }
+      }
+
+    case "M+":
+      if (!operator) return (thirdOperand = firstOperand);
+      if (operator) return (thirdOperand = secondOperand);
+
+    case "M-":
+      return (thirdOperand = "");
+  }
+};
+
+const calculate = () => {
+  switch (operator) {
+    case "+":
+      return add();
+    case "x":
+      return multiply();
+    case "÷":
+      return divide();
+    case "-":
+      return subtract();
+  }
+};
+
+const add = () => `${Number(firstOperand) + Number(secondOperand)}`;
+const multiply = () => `${Number(firstOperand) * Number(secondOperand)}`;
+const divide = () => `${Number(firstOperand) / Number(secondOperand)}`;
+const subtract = () => `${Number(firstOperand) - Number(secondOperand)}`;
